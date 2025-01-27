d4c0d36ca7e6b3999d99a6694194e89b990e335b
PrinceShlime <aryashreepradhan@gmail.com>
2025-01-27 18:13:47 -0500

    multiple huzz

--- huzz.py
+++ huzz.py
@@ -1,2 +1,4 @@
-print("Salutations huzz")
-print("Salutations fine shyt")
+huzz = int(input("How many huzz must you greet?"))
+for i in range(huzz):
+    print("Salutations huzz")
+    print("Salutations fine shyt\n")
